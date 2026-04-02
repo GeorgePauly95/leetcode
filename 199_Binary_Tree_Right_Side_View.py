6037a3d7a645452ed4fd7eac6636b5e772102a14
GeorgePauly95 <georgepauly1995@gmail.com>
2026-04-02 12:29:45 +0530

    solved - BFsolved - BFS
    using dict instead of a list to store the result since it doesn't have
    to be initialized with None values before assigning the right most node
    at each level.

---
+++ 199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        queue, result = [], {}
+        if root is None:
+            return []
+        queue.append((root, 0))
+        while len(queue) != 0:
+            node, level = queue.pop(0)
+            if node is not None:
+                left_node, right_node = node.left, node.right
+                queue.append((left_node, level + 1))
+                queue.append((right_node, level + 1))
+                result[level] = node.val
+        return list(result.values())
