0b277f763545154c54858ea85ebb1151eed1e7a5
GeorgePauly95 <georgepauly1995@gmail.com>
2026-04-06 19:05:23 +0530

    solved - Recursive DFS

--- 199_Binary_Tree_Right_Side_View.py
+++ 199_Binary_Tree_Right_Side_View.py
@@ -8,6 +8,7 @@ class TreeNode:
         self.right = right
 
 
+# use an array instead of a dictionary for result. Can use built-in insert method on array.
 class BFSSolution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         queue, result = [], {}
@@ -38,3 +39,14 @@ class DFSSolution:
                 stack.append((left_node, level + 1))
                 result[level] = node.val
         return list(result.values())
+
+
+class RecursiveDFSSolution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        left_node, right_node = root.left, root.right
+        right_result = self.rightSideView(right_node)
+        left_result = self.rightSideView(left_node)
+        right_length = len(right_result)
+        return [root.val] + right_result + left_result[right_length:]
