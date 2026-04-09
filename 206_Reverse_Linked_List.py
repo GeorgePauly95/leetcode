b5ef8485ab941a074ed3535d8dd145972bc5dd09
GeorgePauly95 <georgepauly1995@gmail.com>
2026-04-09 11:27:02 +0530

    solved - recursive

--- 206_Reverse_Linked_List.py
+++ 206_Reverse_Linked_List.py
@@ -7,7 +7,25 @@ class ListNode:
         self.next = next
 
 
-class Solution:
+class RecursiveSolution:
+    def reverseList(
+        self, head: Optional[ListNode], previous_node=None
+    ) -> Optional[ListNode]:
+        current_node = head
+        if current_node is None:
+            return None
+        elif current_node.next is None:
+            current_node.next = previous_node
+            return current_node
+        else:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            return self.reverseList(current_node, previous_node)
+
+
+class IterativeSolution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         previous_node = None
         current_node = head
