46827f713d0d5fe84cd7864f366f1065bf9732ce
GeorgePauly95 <georgepauly1995@gmail.com>
2026-04-09 10:21:27 +0530

    solved - iterative

---
+++ 206_Reverse_Linked_List.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous_node = None
+        current_node = head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+        return previous_node
