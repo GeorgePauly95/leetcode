bfd181511b73ee6e0b0c9f31625f933f3eb205b0
George <georgepauly1995@gmail.com>
2026-04-10 15:52:07 +0530

    redundant code

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -19,15 +19,13 @@ class Solution:
                 list1 = list1.next
                 if list1 is None:
                     break
-                head.next = ListNode()
-                head = head.next
             else:
                 head.val = list2.val
                 list2 = list2.next
                 if list2 is None:
                     break
-                head.next = ListNode()
-                head = head.next
+            head.next = ListNode()
+            head = head.next
         if list1 is None and list2 is not None:
             head.next = list2
         elif list1 is not None and list2 is None:
