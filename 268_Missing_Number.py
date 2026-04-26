c8d55970f4aed3d7e1833a3d57a595d7edc86c56
GeorgePauly95 <georgepauly1995@gmail.com>
2026-04-26 12:16:07 +0530

    solved - Constant space solution

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -10,3 +10,12 @@ class LinearSpaceSolution:
         for candidate in result:
             if candidate != -1:
                 return candidate
+
+
+class ConstantSpaceSolution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum = n * (n + 1) / 2
+        for num in nums:
+            sum -= num
+        return int(sum)
